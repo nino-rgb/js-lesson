5b470e4c76aa4c2780d832237693c76680edcd3e
nino <pnekoneko907@gmail.com>
2024-08-17 17:53:31 +0900

    initial commit

---
+++ kadai1.js
@@ -0,0 +1,120 @@
+
+//課題１
+// for (let i = 1; i < 101; i++) {
+//   const num1 = i % 2;
+//   const str1 = "奇数です";
+//   const str2 = "偶数です";
+//   if (num1 == 1) {
+//     console.log(i + str1);
+//   }
+//   else {
+//     console.log(i + str2);
+//   }
+// }
+
+
+// const num1 = 1 + 2; // 足し算
+// const num2 = 3 - 1; // 引き算
+// const num3 = 2 * 3; // かけ算
+// const num4 = 6 / 2; // 割算
+// const num5 = 5 % 2; // あまり
+
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+// console.log(num4);
+// console.log(num5);
+
+//課題２
+
+// const a = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const aa = [];
+// for (const num of a) {
+//   aa.push(num * 1);
+// }
+// console.log(aa);
+// const b = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const bb = [];
+// for (const num of b) {
+//   bb.push(num * 2);
+// }
+// console.log(bb);
+// const c = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const cc = [];
+// for (const num of c) {
+//   cc.push(num * 3);
+// }
+// console.log(cc);
+// const d = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const dd = [];
+// for (const num of d) {
+//   dd.push(num * 4);
+// }
+// console.log(dd);
+// const e = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const ee = [];
+// for (const num of e) {
+//   ee.push(num * 5);
+// }
+// console.log(ee);
+// const f = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const ff = [];
+// for (const num of f) {
+//   ff.push(num * 6);
+// }
+// console.log(ff);
+// const g = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const gg = [];
+// for (const num of g) {
+//   gg.push(num * 7);
+// }
+// console.log(gg);
+// const h = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const hh = [];
+// for (const num of h) {
+//   hh.push(num * 8);
+// }
+// console.log(hh);
+// const i = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const ii = [];
+// for (const num of i) {
+//   ii.push(num * 9);
+// }
+// console.log(ii);
+
+
+
+for (let i = 1; i < 10; i++) {
+  const a = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const aa = [];
+  for (const num of a) {
+    aa.push(num * i);
+  }
+  console.log(aa);
+}
+
+
+
+
+
+
+
+//課題３
+
+// for (let i = 1; i < 101; i++) {
+//   const foo2 = i % 3;
+//   const foo3 = i % 5;
+//   if (foo2 == 0 && foo3 == 0) {
+//     console.log("fizzbuzz");
+//   }
+//   else if (foo3 == 0) {
+//     console.log("buzz");
+//   }
+//   else if (foo2 == 0) {
+//     console.log("fizz");
+//   }
+//   else {
+//     console.log(i);
+//   }
+// }
+
---
+++ program study.js
@@ -0,0 +1,129 @@
+
+//関数
+
+// function sampleFunc() {
+//  const str = "sample";
+//  console.log(str);
+// }
+
+// sampleFunc();
+
+// function sampleFunc2(num1, num2) {
+//  const result = num1 * num2;
+//  return result;
+// }
+
+// const res = sampleFunc2(3,8);
+// console.log(res);
+
+
+
+//繰り返し処理
+
+// // for文
+// for(let i =  0; i < 9; i++){
+//     const num = i * 2;
+//     console.log(num);
+// }
+
+//　forof文
+
+// 　
+
+
+
+// //演算子
+// const num1 = 1 + 2;
+// const num2 = 3 - 1;
+// const num3 = 2 * 3;
+// const num4 = 6 / 2;
+// const num5 = 5 % 2;
+ 
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+// console.log(num4);
+// console.log(num5);
+
+//変数を定義するときの違い
+//　constは値の更新ができない変数
+
+
+
+
+
+// //条件分岐
+// const num = 3;
+
+// if (num == 1){
+//   console.log("num は 1 です");
+// }
+// else if(num == 2){
+//   console.log("num は 2 です");
+// }
+// else if(num == 3){
+//     console.log("num は 3 です");
+//   }
+// else {
+//      console.log("num は 1,2,3 ではありません");
+// }
+
+// const num = 1;
+
+// switch (num) {
+//   case 1:
+//      console.log("num は 1 です")
+
+
+
+// }
+
+
+
+
+
+// //オブジェクト型　（連想配列）
+// const user = {
+//  name: "竹内　甫",
+//  age: 31,
+//  email: "hajime@example.com",
+// };
+
+// console.log(user.name);
+// console.log(user.age);
+// console.log(user.email);
+
+
+
+// //配列
+// const array = [0,1,2];
+
+// console.log(array[0]);
+// console.log(array[1]);
+// console.log(array[2]);
+
+
+// const arratStr = ["りんご", "ゴリラ","ラッパ"]
+// console.log(arrayStr[0]);
+// console.log(arrayStr[1]);
+// console.log(arrayStr[2]);
+
+// //文字列型変数の定義
+// const foo = "ほげほげ";
+// const bar = "ふがふが";
+// const ggg = foo + bar;
+
+// //数字型変数の定義
+// const num = 123;
+// const num2 = 456;
+
+// //文字列をコンソールに出力
+// console.log(ggg);
+
+// //数値をコンソールに出力
+// console.log(num+num2);
+
+// //文字列と数値を足し算
+// console.log(foo + num);
+
+
